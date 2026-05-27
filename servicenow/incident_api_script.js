1627dc2507eae97dc624d4e65264ad66760323d0
ManoharCH <145912253+manoharch127@users.noreply.github.com>
2026-05-27 20:47:55 +0530

    Update incident_api_script.js

--- servicenow/incident_api_script.js
+++ servicenow/incident_api_script.js
@@ -1,1 +1,28 @@
+(function process(request, response) {
 
+var data = request.body.data;
+
+var inc = new GlideRecord('incident');
+inc.initialize();
+
+inc.short_description = "AWS Alert: " + data.AlarmName;
+inc.description = JSON.stringify(data);
+
+if(data.State == "ALARM"){
+    inc.priority = 1;
+} else {
+    inc.priority = 3;
+}
+
+inc.assignment_group = "Cloud Support";
+
+var sysId = inc.insert();
+
+response.setStatus(200);
+
+response.setBody({
+    message: "Incident Created",
+    sys_id: sysId
+});
+
+})(request, response);
